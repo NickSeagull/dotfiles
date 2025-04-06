6316557c3f77f42acfed51a54011049163a8fbe6
Nikita Tchayka <github@nickseagull.dev>
2025-04-06 22:03:05 +0100

    Update

---
+++ .hm/emacs/doom.d/packages.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! catppuccin-theme)
+
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+
+(package! aidermacs)
+
+(package! clipetty)
