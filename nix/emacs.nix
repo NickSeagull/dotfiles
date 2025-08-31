18fa4f130ad87996b7d092055345cf144ef87313
Nikita Tchayka <github@nickseagull.dev>
2025-08-31 15:25:48 +0100

    bump

--- nix/emacs.nix
+++ nix/emacs.nix
@@ -2,15 +2,33 @@
 
 {
   home.packages = with pkgs; [
-    nixfmt-rfc-style
+    # Indexing / search dependencies
+    fd
+    (ripgrep.override { withPCRE2 = true; })
+
+    # Font / icon config
+    # Added FiraCode as an example, it's not used in the config example.
+    emacs-all-the-icons-fonts
+    fontconfig
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+
+    nixfmt-rfc-style # :lang nix
   ];
+
+  # Required to autoload fonts from packages installed via Home Manager
+  fonts.fontconfig.enable = true;
 
   home.file = {
   };
 
   home.sessionVariables = {
     EDITOR = "emacsclient -c";
+    DOOMDIR = "${config.xdg.configHome}/doom";
+    EMACSDIR = "${config.xdg.configHome}/emacs";
+    DOOMLOCALDIR = "${config.xdg.dataHome}/doom";
+    DOOMPROFILELOADFILE = "${config.xdg.stateHome}/doom-profiles-load.el";
   };
+  home.sessionPath = [ "${config.xdg.configHome}/emacs/bin" ];
 
   services = {
     emacs = {
@@ -23,5 +41,14 @@
     emacs = {
       enable = true;
     };
+  };
+
+  # Note! This must match $DOOMDIR
+  xdg.configFile."doom".source = ../doom;
+
+  # Note! This must match $EMACSDIR
+  xdg.configFile."emacs".source = builtins.fetchGit {
+    url = "https://github.com/doomemacs/doomemacs.git";
+    rev = "1dae2bf916ea631ab0d129218cf69ece94a11e4f";
   };
 }
