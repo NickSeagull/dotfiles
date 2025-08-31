a1b598bd89f7e16988c6ff2c829475476b06298d
Nikita Tchayka <github@nickseagull.dev>
2025-08-31 13:16:15 +0100

    upd

--- homeenv/flake.nix
+++ homeenv/flake.nix
@@ -8,24 +8,21 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+    { nixpkgs, home-manager, flake-utils, ... }:
     {
-      homeConfigurations."nick" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      homeConfigurations = {
+        "nick" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          modules = [ ./home.nix ];
+        };
+        "nick@darwin" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+          modules = [ ./home.nix ];
+        };
       };
     };
 }
